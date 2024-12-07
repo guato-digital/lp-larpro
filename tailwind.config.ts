2f4f722f71515425ad5697cf29cbd47621628878
Bruno Alexandre <brunoalencaralexandre@gmail.com>
2024-12-07 01:38:05 -0300

    feat: create menu

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,34 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#6759FF",
+        secondary: "#FC944D",
+        lightGray: "#F9F9F9",
+        black: {
+          100: "#D1D3D4",
+          200: "#535763",
+          300: "#2F3643",
+          400: "#29303C",
+          500: "#1A1B2D",
+          card: "#18202E",
+        },
+        accent: {
+          100: "#FFBC99",
+          200: "#CABDFF",
+          300: "#B1E5FC",
+          400: "#B5EBCD",
+          500: "#FFD88D",
+          600: "#CBEBA4",
+          700: "#FB9B9B",
+          800: "#F8B0ED",
+          900: "#AFC6FF",
+        },
+      },
+      width: {
+        desk: "1440px",
+      },
+      maxWidth: {
+        desk: "1440px",
       },
     },
   },
