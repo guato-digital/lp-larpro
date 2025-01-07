28659020f230ab76521f1e7a38810e3412d79399
Bruno Alexandre <brunoalencaralexandre@gmail.com>
2025-01-06 21:24:34 -0300

    chore: remove unused icon

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import * as AccordionPrimitive from "@radix-ui/react-accordion";
 
 import { cn } from "@/lib/utils";
-import { Minus, Plus } from "@phosphor-icons/react/dist/ssr";
+import { Plus } from "@phosphor-icons/react/dist/ssr";
 
 const Accordion = AccordionPrimitive.Root;
 
